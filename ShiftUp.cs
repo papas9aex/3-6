6c016a72ad9796258ce5ca8b43f7d646476e7505
sss <137614601+papas9aex@users.noreply.github.com>
2023-11-01 03:26:30 +0300

    Add files via upload

---
+++ ShiftUp.cs
@@ -0,0 +1,48 @@
+﻿private void ShiftUp(int index)
+{
+    int parentIndex = (index - 1) / 2;
+    while (parentIndex >= 0 && heap[parentIndex] > heap[index])
+    {
+        Swap(parentIndex, index);
+        index = parentIndex;
+        parentIndex = (index - 1) / 2;
+    }
+}
+
+
+
+private void ShiftDown(int index)
+{
+    while (true)
+    {
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+        int largest = index;
+
+
+
+        if (leftChildIndex < size && heap[leftChildIndex] < heap[largest])
+        {
+            largest = leftChildIndex;
+        }
+
+
+
+        if (rightChildIndex < size && heap[rightChildIndex] < heap[largest])
+        {
+            largest = rightChildIndex;
+        }
+
+
+
+        if (largest != index)
+        {
+            Swap(largest, index);
+            index = largest;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
